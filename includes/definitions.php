bb2a2ae441f5a249a93be3af43ccdba9c541db1e
Justin York <justincyork@gmail.com>
2012-09-07 15:20:20 -0600

    More nav touchups; more example home page content

--- includes/definitions.php
+++ includes/definitions.php
@@ -1,15 +1,18 @@
-<div class="well">
-    <div class='row' id="definitions">
-        <div class='span4'>
-            <?php include 'includes/rootsdef.php'; ?>
+<section id="definitions">
+    <h2>Define</h3>
+    <div class="well">
+        <div class="row">
+            <div class="span4">
+                <?php include 'includes/rootsdef.php'; ?>
+            </div>
+            <div class="span4">
+                <?php include 'includes/developdef.php'; ?>
+            </div>
         </div>
-        <div class='span4'>
-            <?php include 'includes/developdef.php'; ?>
+        <div class="row">
+            <div class="span8">
+                <?php include 'includes/rootsdevdef.php'; ?>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class='span8'>
-            <?php include 'includes/rootsdevdef.php'; ?>
-        </div>
-    </div>
-</div>
+</section>
--- includes/homepagesubnav.php
+++ includes/homepagesubnav.php
@@ -1,10 +1,11 @@
 
 <div class="container">
-    <h1>Home</h1>
     
     <div class="span3 subnav-wrap">
-        <ul class="nav nav-tabs nav-stacked subnav" data-spy="affix" data-offset-top="0">
+        <h1>Home</h1>
+        <ul class="nav nav-tabs nav-stacked subnav" data-spy="affix" data-offset-top="10">
             <li><a href="#definitions">Define RootsDev</a></li>
+            <li><a href="#getinvolved">Get Involved</a></li>
             <li><a href="#resources">Resources</a></li>
         </ul>
     </div>
@@ -13,11 +14,27 @@
     
         <?php include 'definitions.php';?>
         
-        <div id="resources">
+        <section id="getinvolved">
+            <h2>Get Involved</h2>
             <div class="well">
-                <p>some stuff to read</p>
+              <p><a href="https://github.com/rootsdev">RootsDev on GitHub</a></p>
+              <p><a href="https://groups.google.com/forum/?fromgroups#!forum/rootsdev">RootsDev Mailing List</a></p>
             </div>
-        </div>
+        </section>
         
+        <section id="resources">
+            <h2>Resources</h2>
+            <div class="well">
+                <h3>Open Source Family Tree Projects</h3>
+                <ul>
+                    <li><a href="https://github.com/cmosher01/Gedcom-Web-View/">Gedcom-Web-View</a></li>
+                    <li><a href="http://opensource.geneanet.org/projects/geneweb">Geneweb</a></li>
+                    <li><a href="http://gramps-project.org/">Gramps</a></li>
+                    <li><a href="http://www.humo-gen.com/software/">HuMo-gen</a></li>
+                    <li><a href="http://lifelines.sourceforge.net/">LifeLines</a></li>
+                    <li><a href="http://webtrees.net/index.php">webtrees</a></li>
+                <ul>
+            </div>
+        </section>
     </span>
 </div>
