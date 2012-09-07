da57faa21808258e8624060f6ad775a43f89ac0e
Justin York <justincyork@gmail.com>
2012-09-07 15:47:57 -0600

    Rewrite about page; add my own info

--- developers/david-tanner.php
+++ developers/david-tanner.php
@@ -1,26 +1,16 @@
-<div class="well bio">  
-  <table id="davidtanner" class="table">
-    <tbody>
-      <tr>
-          <th>Name:</th>
-          <td>David Tanner</td>
-      </tr>
-      <tr>
-          <th>Occupation:</th>
-          <td>System Test Engineer for a Telematics company</td>
-      </tr>
-      <tr>
-          <th>Contact Info:</th>
-          <td><a href="mailto:david.tanner@rootsdev.org?subject=feedback">david.tanner@rootsdev.org</a></td>
-      </tr>
-      <tr>
-          <th>About Me:</th>
-          <td>
-              <p>I am an Electrical Engineering student at Brigham Young University.</p> 
-              <p>I am huge fan of Star Wars, but I think my daughter might be bigger, and she is only 2.</p>
-              <p>I am the admin for the rootsdev.org server, and am excited to be involved in the new rootsdev world.</p> 
-          </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+<tr>
+    <th>Occupation:</th>
+    <td>System Test Engineer for a Telematics company</td>
+</tr>
+<tr>
+    <th>Contact Info:</th>
+    <td><a href="mailto:david.tanner@rootsdev.org?subject=feedback">david.tanner@rootsdev.org</a></td>
+</tr>
+<tr>
+    <th>About Me:</th>
+    <td>
+        <p>I am an Electrical Engineering student at Brigham Young University.</p> 
+        <p>I am huge fan of Star Wars, but I think my daughter might be bigger, and she is only 2.</p>
+        <p>I am the admin for the rootsdev.org server, and am excited to be involved in the new rootsdev world.</p> 
+    </td>
+</tr>
