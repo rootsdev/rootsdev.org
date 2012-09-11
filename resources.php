3a3ef782fadd90e05e3b2044ac7c8c32acf8fc69
Justin York <justincyork@gmail.com>
2012-09-11 08:24:58 -0600

    Add more resources

--- resources.php
+++ resources.php
@@ -6,12 +6,15 @@
         <h1>Resources</h1>
         <ul class="nav nav-tabs nav-stacked subnav" data-spy="affix" data-offset-top="10">
             <li><a href="#opensource-trees">Open Source Family Tree Projects</a></li>
+			<li><a href="#opensource-other">Open Source Genealogy Projects</a></li>
+			<li><a href="#apis">Genealogy APIs</a></li>
+			<li><a href="#standards">Genealogy Standards and Standards Groups</a></li>
         </ul>
     </div>
     
     <div class="span9">
       
-      <section id="opensource-trees">
+		<section id="opensource-trees">
             <h2>Open Source Family Tree Projects</h2>
             <div class="well">
                 <ul>
@@ -24,7 +27,46 @@
                 <ul>
             </div>
         </section>
-        
+		
+		<section id="opensource-other">
+			<h2>Open Source Genealogy Projects</h2>
+			<div class="well">
+                <ul>
+                    <li><a href="https://github.com/cmosher01/Gedcom-Web-View/">Gedcom-Web-View</a></li>
+                    <li><a href="https://github.com/DallanQ/Places">Places</a></li>
+					<li><a href="https://github.com/DallanQ/Names">Names</a></li>
+					<li><a href="">Gedcom</a></li>
+					<li>FamilySearch login boilerplate: <a href="https://github.com/DallanQ/familysearch-boilerplate-client">client</a> and <a href="https://github.com/DallanQ/familysearch-boilerplate-server">server</a></li>
+					<li><a href="https://github.com/FreeUKGen">FreeUKGen</a></li>
+					<li><a href="http://www.leafseek.com/">LeafSeek</a> - an open source toolkit for building genealogical and historical record search engines, based on Apache Solr (back-end) and Solarium/PHP/HTML/CSS/jQuery (front-end)</li>
+                <ul>
+            </div>
+        </section>
+		
+		<section id="apis">
+			<h2>Genealogy APIs</h2>
+			<div class="well">
+                <ul>
+					<li><a href="">FamilySearch API</a></li>
+					<li><a href="">Geni.com API</a></li>
+					<li><a href="">MyHeritage Family Graph API</a></li>
+				</ul>
+			</div>
+		</section>
+		
+		<section id="standards">
+			<h2>Genealogy Standards and Standards Groups</h2>
+			<div class="well">
+                <ul>
+					<li><a href="">FHISO</a></li>
+					<li><a href="">BetterGEDCOM</a></li>
+					<li><a href="">schema.org/historical-data</a> (microformat)</li>
+					<li><a href="">GEDCOM</a></li>
+					<li><a href="">GEDCOMX</a></li>
+				</ul>
+			</div>
+		</section>
+
     </div>
 
 <?php include 'includes/footer.php'; ?>
