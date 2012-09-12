fd8548889e5927282ceabe8ea203dc63e37d3ef0
David Tanner <dtanner@inthinc.com>
2012-09-12 17:14:16 -0600

    Reverse the order.  Get involved should be first

--- includes/home/subnav.php
+++ includes/home/subnav.php
@@ -4,15 +4,13 @@
     <div class="span3 subnav-wrap">
         <h1>Home</h1>
         <ul class="nav nav-tabs nav-stacked subnav" data-spy="affix" data-offset-top="10">
-            <li><a href="#definitions">Define RootsDev</a></li>
             <li><a href="#getinvolved">Get Involved</a></li>
+            <li><a href="#definitions">Define RootsDev</a></li>
         </ul>
     </div>
     
     <div class="span9">
-    
-        <?php include('definitions.php');?>
-        
+
         <section id="getinvolved">
             <h2>Get Involved</h2>
             <div class="well">
@@ -20,6 +18,8 @@
               <p><a href="https://groups.google.com/forum/?fromgroups#!forum/rootsdev">RootsDev Mailing List</a></p>
             </div>
         </section>
+        
+        <?php include('definitions.php');?>
         
     </span>
 </div>
