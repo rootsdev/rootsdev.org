651a317af190459aedff919fa7587ee860149dff
Christopher Mosher <christopher.mosher@surveysampling.com>
2012-10-07 16:38:05 -0400

    slight spelling and grammar corrections

--- includes/home/def-rootsdev.php
+++ includes/home/def-rootsdev.php
@@ -5,7 +5,7 @@
     <div class="span" style="width:90px;">Proper Noun:</div>
     <div class="span6">
         <ol>
-            <li style="list-style-type:decimal">A community dedicated to advancing software in the world of Genealogy so that individuals may strenghen their family trees.</li>
+            <li style="list-style-type:decimal">A community dedicated to advancing software in the world of Genealogy so that individuals may strengthen their family trees.</li>
         </ol>
     </div>
 </div>
@@ -15,7 +15,7 @@
         <ol>
             <li style="list-style-type:decimal">To make genealogy work flows faster and easier.</li>
             <li style="list-style-type:decimal">To provide a list of resources for genealogical research.</li>
-            <li style="list-style-type:decimal">To provide a community support for open source genealogical software.</li>
+            <li style="list-style-type:decimal">To provide community support for open source genealogical software.</li>
         </ol>
     </div>
 </div>
